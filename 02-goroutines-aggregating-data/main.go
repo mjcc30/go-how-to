b2701f651ea5b707a77b261c11768dbecb65fc50
mjcc30 <m.cordeiro@ecole-ipssi.net>
2022-12-18 15:37:47 +0000

    add goroutine code

--- 02-goroutines-aggregating-data/main.go
+++ 02-goroutines-aggregating-data/main.go
@@ -2,14 +2,52 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
-
-// public Hello function which returns a string
-func Hello() string {
-	return "Hello, world"
-}
 
 // the main function
 func main() {
-	fmt.Println(Hello())
+	start := time.Now() // start time
+	userName := FetchUser() // get the user name
+	respch := make(chan any, 2) // create a channel to store the response
+	wg := &sync.WaitGroup{} // create a wait group
+
+	wg.Add(2) // add two goroutines to the wait group
+
+
+	go FetchUserLikes(userName, respch, wg) // fetch user's likes
+	go FetchUserMatch(userName, respch, wg) // fetch user's matches
+
+	wg.Wait() // block until 2 wg.Done()
+	close(respch) // close the channel
+
+	for resp := range respch {
+		fmt.Println("resp: ", resp)
+	}
+
+	fmt.Println("took:", time.Since(start)) // print the elapsed time
+}
+
+// fetchUser fetches user's name
+func FetchUser() string {
+	time.Sleep(time.Millisecond * 100) // sleep for 100 ms
+
+	return "BOB"
+}
+
+// fetchUserLikes is a goroutine that fetches the user's likes
+func FetchUserLikes(userName string, respch chan any, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 150) // sleep for 150ms
+
+	respch <- 11 // send the response to the channel
+	wg.Done()
+}
+
+// fetchUserMatch is a goroutine that fetches the user's matches
+func FetchUserMatch(userName string, respch chan any, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 100) // sleep for 100 ms
+
+	respch <- "ANNA" // send the response to the channel
+	wg.Done()
 }
